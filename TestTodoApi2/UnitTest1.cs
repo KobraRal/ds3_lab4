ff04bd6966f83c8b6ba807c2f71dff5e5fd74abe
KobraRal <gavrilenkodanik1@mail.ru>
2021-06-05 00:07:04 +0300

    Create UnitTest1.cs

---
+++ TestTodoApi2/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TestTodoApi2
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
